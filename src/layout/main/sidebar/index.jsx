fc0e775d1192323402f625fb80b80ee34d822b0e
hesennov <hasanhasanov345@gmail.com>
2023-09-30 19:34:16 +0400

    fix:Layout

--- src/layout/main/sidebar/index.jsx
+++ src/layout/main/sidebar/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Logo from '@/layout/main/sidebar/logo/index'
 const index = () => {
   return (
-    <aside className='w-[275px] min-h-screen'>sidebar</aside>
+    <aside className='w-[275px] min-h-screen'>
+        <Logo/>
+    </aside>
   )
 }
 
