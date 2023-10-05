f1e1d1df801917a3cec88109476d0b1027361f30
hesennov <hasanhasanov345@gmail.com>
2023-10-05 20:52:15 +0400

    a

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const index = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
+    <div className="flex items-center justify-center h-screen bg-blue-400">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
         <p className="text-xl text-gray-600">Oops! Sayfa bulunamadı.</p>
