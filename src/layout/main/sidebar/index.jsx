a63141c79acd98202bd5a8778f664f4af420de62
hesennov <hasanhasanov345@gmail.com>
2023-10-05 12:24:55 +0400

    fix:menu buttons

--- src/layout/main/sidebar/index.jsx
+++ src/layout/main/sidebar/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Logo from '@/layout/main/sidebar/logo/index'
+import Menu from '@/layout/main/sidebar/menu/index'
 const index = () => {
   return (
     <aside className='w-[275px] min-h-screen'>
         <Logo/>
+        <Menu/>
     </aside>
   )
 }
