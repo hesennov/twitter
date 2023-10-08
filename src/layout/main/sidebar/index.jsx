590dacb51949f03e1297672b60d49a7dcfebeae9
hesennov <hasanhasanov345@gmail.com>
2023-10-08 21:11:19 +0400

    a

--- src/layout/main/sidebar/index.jsx
+++ src/layout/main/sidebar/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Logo from '@/layout/main/sidebar/logo/index'
-import Menu from '@/layout/main/sidebar/menu/index'
+import React from "react";
+import Logo from "@/layout/main/sidebar/logo/index";
+import Menu from "@/layout/main/sidebar/menu/index";
 const index = () => {
   return (
-    <aside className='w-[275px] min-h-screen'>
-        <Logo/>
-        <Menu/>
+    <aside className="w-[275px] min-h-screen flex flex-col">
+      <Logo />
+      <Menu />
+      <div className="mt-auto">
+        <div className="my-3">test</div>
+      </div>
     </aside>
-  )
-}
+  );
+};
 
-export default index
+export default index;
